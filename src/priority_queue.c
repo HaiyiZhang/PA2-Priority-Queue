c8191e77e150095e333fc720d928d1a943d3660b
HaiyiZhang <47614988+HaiyiZhang@users.noreply.github.com>
2021-12-12 17:47:50 -0500

    Update priority_queue.c

--- src/priority_queue.c
+++ src/priority_queue.c
@@ -11,7 +11,19 @@
  * nprios: The maximum number of priorities to support
  */
 PriorityQueue *pqueue_init(int nprios) {
-    return NULL;
+     PriorityQueue *pq;
+    if(nprios <= 0){
+        return NULL;
+    }
+    else{
+        pq = (PriorityQueue *) malloc(sizeof(PriorityQueue));
+        pq->tails =(PQNode **)malloc(nprios * sizeof(PQNode *));
+        pq->nprios = nprios;
+        for(int i = 0; i<nprios; i++){
+            pq->tails[i] = NULL;
+        }
+    }
+    return pq;
 }
 
 /*
@@ -22,6 +34,21 @@ PriorityQueue *pqueue_init(int nprios) {
  * pqueue: the queue to free
  */
 void pqueue_free(PriorityQueue *pqueue) {
+    PQNode *head = pqueue->head;
+    if(head != NULL){
+        while(head->next != NULL){
+            head = head->next;
+            free(head->prev);
+        }
+        free(head);
+    }
+    else{
+        free(head);
+    }
+    for(int i = 0; i < pqueue->nprios; i++){
+        free(pqueue->tails[i]);
+    }
+    free(pqueue);
 }
 
 /*
@@ -32,18 +59,96 @@ void pqueue_free(PriorityQueue *pqueue) {
  * priority: the priority at which this value is to be inserted
  */
 void pqueue_insert(PriorityQueue *pqueue, int value, int priority) {
+    if(priority >= pqueue->nprios || priority < 0){
+        ;
+    }
+    else{
+        PQNode *thelast = pqueue->tails[priority];
+        PQNode *myhead = pqueue->head;
+        
+        PQNode *node = (PQNode *)malloc(sizeof(PQNode));
+
+        int checknull = 0;
+        int checkmax = 0;
+        for(int i = 0; i < pqueue->nprios; i++){
+            if(pqueue->tails[i] != NULL){
+                checknull = checknull + 1;
+                if(priority >= pqueue->tails[i]->priority){
+                    checkmax = checkmax + 1;
+                }
+            }
+        }
+        node->value = value;
+        node->priority = priority;
+        pqueue->tails[priority] = node;
+        
+        if(checknull == 0 || checkmax == 0){
+            pqueue->head = node;
+            pqueue->head->next = NULL;
+            pqueue->head->prev = NULL;
+        }
+
+                
+        if(thelast == NULL){
+            if(pqueue->tails[priority] == pqueue->head){
+                pqueue->tails[priority]->prev = NULL;
+                pqueue->tails[priority]->next = myhead;
+                if(myhead != NULL){
+                    myhead->prev = pqueue->tails[priority];}}
+            else{
+                int before = 0;
+                int after = (pqueue->nprios)-1;
+                for(int i = 0; i < priority; i++){    //check later
+                    if(pqueue->tails[i] != NULL){
+                        before = i;}}
+                for(int i = (pqueue->nprios)-1; i > priority; i--){    //check later
+                    if(pqueue->tails[i] != NULL){
+                        after = i;}}
+                if(pqueue->tails[before]!=NULL){
+                    pqueue->tails[priority]->prev = pqueue->tails[before];
+                    pqueue->tails[before]->next = pqueue->tails[priority];}
+                if(pqueue->tails[after]!=NULL){
+                    pqueue->tails[priority]->next = pqueue->tails[after];
+                    pqueue->tails[after]->prev = pqueue->tails[priority];}}}
+        else{
+            pqueue->tails[priority]->next = thelast->next;
+            pqueue->tails[priority]->prev = thelast;
+            thelast->next = pqueue->tails[priority];
+        }
+    }
 }
 
 /*
  * Return the head queue node without removing it.
  */
 PQNode *pqueue_peek(PriorityQueue *pqueue) {
-    return NULL;
+    if(pqueue->head == NULL){
+        return NULL;
+    }
+    else{
+        PQNode *node = pqueue->head;
+        return node;
+    }
+
 }
 
 /*
  * Remove and return the head queue node.
  */
 PQNode *pqueue_get(PriorityQueue *pqueue) {
-    return NULL;
+    if(pqueue->head == NULL){
+        return NULL;
+    }
+    else{
+        PQNode *node = pqueue->head;
+        for(int i = 0; i < pqueue->nprios; i++){
+            if(pqueue->tails[i] == node){
+                pqueue->tails[i] = NULL;
+            }
+        }
+        PQNode *nodenext = pqueue->head->next;
+        pqueue->head = nodenext;
+        pqueue->head->prev = NULL;
+        return node;
+    }
 }
